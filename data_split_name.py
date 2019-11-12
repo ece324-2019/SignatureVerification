5d8ac3c6c45841547a6472a6a0c4264e1dcb96ae
Yize <yize.zhao@hotmail.com>
2019-11-12 02:10:53 -0500

    changed direcotries and splitted to train, test, valid

--- data_split_name.py
+++ data_split_name.py
@@ -13,29 +13,54 @@ target_dir = []
 forg_target_dir = []
 auth_target_dir = []
 
+#create base directory
+os.mkdir("/Users/yizezhao/PycharmProjects/ece324/sigver/signatures/train")
+os.mkdir("/Users/yizezhao/PycharmProjects/ece324/sigver/signatures/valid")
+os.mkdir("/Users/yizezhao/PycharmProjects/ece324/sigver/signatures/test")
+
+
+
 # forgery data
 for i in range(1, 56):
-    forg_target_dir.append("/Users/yizezhao/PycharmProjects/ece324/sigver/signatures/name_" + str(i) + "_forg")
-    print(str(i))
+    if(i <= 6): #valid
+        forg_target_dir.append("/Users/yizezhao/PycharmProjects/ece324/sigver/signatures/valid/name_" + str(i) + "_forg")
 
-    os.mkdir(forg_target_dir[i - 1])
+    elif(i <= 15): #test
+        forg_target_dir.append(
+            "/Users/yizezhao/PycharmProjects/ece324/sigver/signatures/test/name_" + str(i) + "_forg")
+    else:
+        forg_target_dir.append(
+            "/Users/yizezhao/PycharmProjects/ece324/sigver/signatures/train/name_" + str(i) + "_forg")
+    print(forg_target_dir[i-1])
+    os.mkdir(forg_target_dir[i-1])
+
+
 
 with open('forg_path.txt', 'r') as forg_path:
     for cnt, line in enumerate(forg_path):
         line = line.rstrip()
         name = re.match(
             r'/Users/yizezhao/PycharmProjects/ece324/sigver/signatures/full_forg/forgeries_(.*?)_(.*).png', line)
-        # print(name.group(1))
-        dest = forg_target_dir[int(name.group(1)) - 1]
+        #print(name.group(1))
+        dest = forg_target_dir[int(name.group(1))-1]
         print(line)
         print(dest)
         shutil.move(line, dest)
 
-# authetic data
-for i in range(1, num_of_names + 1):
-    auth_target_dir.append("/Users/yizezhao/PycharmProjects/ece324/sigver/signatures/name_" + str(i) + "_auth")
 
-    os.mkdir(auth_target_dir[i - 1])
+
+
+#authetic data
+for i in range(1, num_of_names + 1):
+    if (i <= 6):  # valid
+        auth_target_dir.append("/Users/yizezhao/PycharmProjects/ece324/sigver/signatures/valid/name_" + str(i) + "_auth")
+    elif (i <= 15):  # test
+        auth_target_dir.append(
+            "/Users/yizezhao/PycharmProjects/ece324/sigver/signatures/test/name_" + str(i) + "_auth")
+    else:
+        auth_target_dir.append(
+            "/Users/yizezhao/PycharmProjects/ece324/sigver/signatures/train/name_" + str(i) + "_auth")
+    os.mkdir(auth_target_dir[i-1])
     print(auth_target_dir)
 
 with open('auth_path.txt', 'r') as auth_path:
@@ -44,7 +69,7 @@ with open('auth_path.txt', 'r') as auth_path:
         name = re.match(
             r'/Users/yizezhao/PycharmProjects/ece324/sigver/signatures/full_org/original_(.*?)_(.*).png', line)
         print(name.group(1))
-        dest = auth_target_dir[int(name.group(1)) - 1]
+        dest = auth_target_dir[int(name.group(1))-1]
         print(line)
         print(dest)
-    shutil.move(line, dest)
+        shutil.move(line, dest)
