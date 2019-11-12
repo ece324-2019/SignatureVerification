581819bc35c93d41509a74fe8d5394d8786950fb
Yize <yize.zhao@hotmail.com>
2019-11-12 00:59:28 -0500

    change splitting to parallel direcotries

--- data_split_name.py
+++ data_split_name.py
@@ -13,47 +13,38 @@ target_dir = []
 forg_target_dir = []
 auth_target_dir = []
 
-# for i in range(1, num_of_names + 1):
-#     target_dir.append("/Users/yizezhao/PycharmProjects/ece324/sigver/signatures/name_" + str(i))
-#     print(str(i))
-#
-#     # os.mkdir(target_dir[i-1])
-# 
-# # forgery data
-# for i in range(1, 56):
-#     forg_target_dir.append("/Users/yizezhao/PycharmProjects/ece324/sigver/signatures/name_" + str(i) + "/1")
-#     print(str(i))
-#
-#     #os.mkdir(forg_target_dir[i-1])
-#
-#
-#
-# with open('forg_path.txt', 'r') as forg_path:
-#     for cnt, line in enumerate(forg_path):
-#         line = line.rstrip()
-#         name = re.match(
-#             r'/Users/yizezhao/PycharmProjects/ece324/sigver/signatures/full_forg/forgeries_(.*?)_(.*).png', line)
-#         #print(name.group(1))
-#         dest = forg_target_dir[int(name.group(1))-1]
-#         print(line)
-#         print(dest)
-#         shutil.move(line, dest)
-#
-#
-# #authetic data
-# for i in range(1, num_of_names + 1):
-#     auth_target_dir.append("/Users/yizezhao/PycharmProjects/ece324/sigver/signatures/name_" + str(i) + "/0")
-#
-#     #os.mkdir(auth_target_dir[i-1])
-#     print(auth_target_dir)
-#
-# with open('auth_path.txt', 'r') as auth_path:
-#     for cnt, line in enumerate(auth_path):
-#         line = line.rstrip()
-#         name = re.match(
-#             r'/Users/yizezhao/PycharmProjects/ece324/sigver/signatures/full_org/original_(.*?)_(.*).png', line)
-#         print(name.group(1))
-#         dest = auth_target_dir[int(name.group(1))-1]
-#         print(line)
-#         print(dest)
-#         shutil.move(line, dest)
+# forgery data
+for i in range(1, 56):
+    forg_target_dir.append("/Users/yizezhao/PycharmProjects/ece324/sigver/signatures/name_" + str(i) + "_forg")
+    print(str(i))
+
+    os.mkdir(forg_target_dir[i - 1])
+
+with open('forg_path.txt', 'r') as forg_path:
+    for cnt, line in enumerate(forg_path):
+        line = line.rstrip()
+        name = re.match(
+            r'/Users/yizezhao/PycharmProjects/ece324/sigver/signatures/full_forg/forgeries_(.*?)_(.*).png', line)
+        # print(name.group(1))
+        dest = forg_target_dir[int(name.group(1)) - 1]
+        print(line)
+        print(dest)
+        shutil.move(line, dest)
+
+# authetic data
+for i in range(1, num_of_names + 1):
+    auth_target_dir.append("/Users/yizezhao/PycharmProjects/ece324/sigver/signatures/name_" + str(i) + "_auth")
+
+    os.mkdir(auth_target_dir[i - 1])
+    print(auth_target_dir)
+
+with open('auth_path.txt', 'r') as auth_path:
+    for cnt, line in enumerate(auth_path):
+        line = line.rstrip()
+        name = re.match(
+            r'/Users/yizezhao/PycharmProjects/ece324/sigver/signatures/full_org/original_(.*?)_(.*).png', line)
+        print(name.group(1))
+        dest = auth_target_dir[int(name.group(1)) - 1]
+        print(line)
+        print(dest)
+    shutil.move(line, dest)
