2e9ad34a6740fc815b73a0c96a957ef5019be91e
Yize <yize.zhao@hotmail.com>
2019-11-15 01:45:24 -0500

    generate a smaller dataset for overfit validation

--- models.py
+++ models.py
@@ -24,7 +24,6 @@ class SiameseNetwork(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(3, stride=2),
             nn.Dropout2d(p=0.3),
-
         )
 
         # Defining the fully connected layers
@@ -50,4 +49,61 @@ class SiameseNetwork(nn.Module):
         output1 = self.forward_once(input1)
         # forward pass of input 2
         output2 = self.forward_once(input2)
+        return output1, output2
+
+
+
+class TripletNetwork(nn.Module):
+    def __init__(self):
+        super(TripletNetwork, self).__init__()
+
+        # Setting up the Sequential of CNN Layers
+        self.cnn1 = nn.Sequential(
+
+            nn.Conv2d(1, 96, kernel_size=11, stride=1),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(5, alpha=0.0001, beta=0.75, k=2),
+            nn.MaxPool2d(3, stride=2),
+
+            nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(5, alpha=0.0001, beta=0.75, k=2),
+            nn.MaxPool2d(3, stride=2),
+            nn.Dropout2d(p=0.3),
+
+            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, stride=2),
+            nn.Dropout2d(p=0.3),
+        )
+
+        # Defining the fully connected layers
+        self.fc1 = nn.Sequential(
+            nn.Linear(30976, 1024),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.5),
+
+            nn.Linear(1024, 128),
+            nn.ReLU(inplace=True),
+
+            nn.Linear(128, 2))
+
+    def forward_once(self, x):
+        # Forward pass
+        output = self.cnn1(x)
+        output = output.view(output.size()[0], -1)
+        output = self.fc1(output)
+        return output
+
+    def forward(self, input1, input2, input3):
+        # output for anchor
+        output1 = self.forward_once(input1)
+        # output for positive
+        output2 = self.forward_once(input2)
+        # output for negative
+        output3 = self.forward_once(input3)
+
+
         return output1, output2
