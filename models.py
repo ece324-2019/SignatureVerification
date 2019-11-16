c2892bd332dab82f48cec331944ad29d81b68c07
terrymei <zhixinterry@gmail.com>
2019-11-16 15:24:03 -0500

    integrate vgg16_bn into the baseline model

--- models.py
+++ models.py
@@ -1,4 +1,9 @@
 import torch.nn as nn
+import torch
+from torchvision import models
+from torch.autograd import Variable
+import copy
+
 
 class SiameseNetwork(nn.Module):
     def __init__(self):
@@ -50,7 +55,6 @@ class SiameseNetwork(nn.Module):
         # forward pass of input 2
         output2 = self.forward_once(input2)
         return output1, output2
-
 
 
 class TripletNetwork(nn.Module):
@@ -106,3 +110,77 @@ class TripletNetwork(nn.Module):
         output3 = self.forward_once(input3)
 
         return output1, output2, output3  #CALLER: output = triplet_loss(anchor, positive, negative)
+
+
+class VGG_SiameseNet(nn.Module):
+    def __init__(self):
+        super(VGG_SiameseNet, self).__init__()
+
+        # transfer learning
+        self.vgg16 = models.vgg16_bn(pretrained=True)
+        # self.vgg16.load_state_dict(torch.load("../input/vgg16bn/vgg16_bn.pth"))
+        for param in self.vgg16.features.parameters():
+            param.require_grad = False
+        features = list(self.vgg16.classifier.children())[:-7]
+        self.vgg16.classifier = nn.Sequential(*features)  # Replace the model classifier
+        features2 = list(self.vgg16.features.children())
+        self.vgg16.features = nn.Sequential(*features2)
+        # features3 = list(self.vgg16.avgpool.children())[:-1]
+        # self.vgg16.avgpool = nn.Sequential(*features3)
+        # print(self.vgg16)
+
+        # Setting up the Sequential of CNN Layers
+        self.cnn1 = nn.Sequential(
+            nn.Conv2d(1, 96, kernel_size=3, stride=1),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(5, alpha=0.0001, beta=0.75, k=2),
+            nn.MaxPool2d(3, stride=2),
+
+            nn.Conv2d(96, 256, kernel_size=3, stride=1, padding=2),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(5, alpha=0.0001, beta=0.75, k=2),
+            nn.MaxPool2d(3, stride=2),
+            nn.Dropout2d(p=0.3),
+
+            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, stride=2),
+            nn.Dropout2d(p=0.3),
+        )
+
+        # Defining the fully connected layers
+        self.fc1 = nn.Sequential(
+            nn.Linear(25088, 1024),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.5),
+
+            nn.Linear(1024, 128),
+            nn.ReLU(inplace=True),
+
+            nn.Linear(128, 10))
+
+    def forward_once(self, x):
+        # Forward pass
+        x = x.permute([1, 0, 2, 3])
+        x_copy1 = copy.deepcopy(x)
+        x_copy2 = copy.deepcopy(x)
+        x = torch.cat((x, x_copy1, x_copy2), 0)
+        x = x.permute([1, 0, 2, 3])
+        output = self.vgg16(x)
+        # output = torch.reshape(output, (output.size()[0], 1, output.size()[1], 1))
+        # output = self.cnn1(output)
+        output = output.view(output.size()[0], -1)
+        output = self.fc1(output)
+        return output
+
+    def forward(self, input1, input2):
+        # forward pass of input 1
+        output1 = self.forward_once(input1)
+        # forward pass of input 2
+        output2 = self.forward_once(input2)
+        return output1, output2
+
+
+test = VGG_SiameseNet()
