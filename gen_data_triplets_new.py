3b52ea58919d164f4cf4aef399d7e2941d0d58c1
Yize <yize.zhao@hotmail.com>
2019-11-27 21:56:07 -0500

    new triplet list

--- gen_data_triplets_new.py
+++ gen_data_triplets_new.py
@@ -29,26 +29,26 @@ command for getting smaller dataset:
 '''
 
 base_dir = '/Users/yizezhao/PycharmProjects/ece324/sigver/'
-test_auth_path = base_dir + 'sigcomp_test_orig.txt'
-test_forg_path = base_dir + 'sigcomp_test_forg.txt'
+test_auth_path = base_dir + 'sigcomp_cn_train_orig.txt'
+test_forg_path = base_dir + 'sigcomp_cn_train_forg.txt'
 
 
 
-test_triplet_path = '/Users/yizezhao/PycharmProjects/ece324/sigver/test_triplet_list_new.csv'
+test_triplet_path = '/Users/yizezhao/PycharmProjects/ece324/sigver/train_triplet_list_cn.csv'
 
-names = ['001', '002', '003', '004', '006', '009', '012', '014', '015', '016']
+names = ['001', '002', '003', '004', '005', '006', '007', '008', '009', '010']
 
 
-test_forg_names = []
-test_auth_names = []
+train_forg_names = []
+train_auth_names = []
 
 for i, id in enumerate(names):
-    test_forg_names.append(base_dir + 'temp_lists/test_forg_' + id + '.txt')
-    print(test_forg_names[i])
-    open(test_forg_names[i], 'a').close()
-    test_auth_names.append(base_dir + 'temp_lists/test_auth_' + id + '.txt')
-    print(test_auth_names[i])
-    open(test_auth_names[i], 'a').close()
+    train_forg_names.append(base_dir + 'temp_lists/train_forg_' + id + '.txt')
+    print(train_forg_names[i])
+    open(train_forg_names[i], 'a').close()
+    train_auth_names.append(base_dir + 'temp_lists/train_auth_' + id + '.txt')
+    print(train_auth_names[i])
+    open(train_auth_names[i], 'a').close()
     print(i)
 
 '''
@@ -106,32 +106,32 @@ START GENERATING NAME LISIS
 #             temp.write(line + '\n')
 #             print(forg_names[int(name.group(1))-1])
 #             print(line)
-#
-# with open(test_auth_path, 'r') as t_auth_path:
-#     for cnt, line in enumerate(t_auth_path):
-#         line = line.rstrip()
-#         #sigComp2011-trainingSet-bi/016_forg/0202016_04.png
-#         #sigComp2011-trainingSet-bi/016_orig/016_24.PNG
-#         name = re.match(
-#             r'sigComp2011-trainingSet-bi/(.*?)_orig/.*', line)
-#         print("index", name.group(1))
-#         with open(base_dir + 'temp_lists/test_auth_' + name.group(1) + '.txt', 'a') as temp:
-#             temp.write(line + '\n')
-#             print(base_dir + 'temp_lists/test_auth_' + name.group(1) + '.txt')
-#             print(line)
-#
-# with open(test_forg_path, 'r') as t_forg_path:
-#     for cnt, line in enumerate(t_forg_path):
-#         line = line.rstrip()
-#         #sigComp2011-trainingSet-bi/016_forg/0202016_04.png
-#         #sigComp2011-trainingSet-bi/016_orig/016_24.PNG
-#         name = re.match(
-#             r'sigComp2011-trainingSet-bi/(.*?)_forg/.*', line)
-#         print("index", name.group(1))
-#         with open(base_dir + 'temp_lists/test_forg_' + name.group(1) + '.txt', 'a') as temp:
-#             temp.write(line + '\n')
-#             print(base_dir + 'temp_lists/test_forg_' + name.group(1) + '.txt')
-#             print(line)
+
+with open(test_auth_path, 'r') as t_auth_path:
+    for cnt, line in enumerate(t_auth_path):
+        line = line.rstrip()
+        #sigComp2011-trainingSet-bi/016_forg/0202016_04.png
+        #sigComp2011-trainingSet-bi/016_orig/016_24.PNG
+        name = re.match(
+            r'sigComp2011-trainingSet-CN/cn_(.*?)_orig/.*', line)
+        print("index", name.group(1))
+        with open(base_dir + 'temp_lists/train_auth_' + name.group(1) + '.txt', 'a') as temp:
+            temp.write(line + '\n')
+            print(base_dir + 'temp_lists/train_auth_' + name.group(1) + '.txt')
+            print(line)
+
+with open(test_forg_path, 'r') as t_forg_path:
+    for cnt, line in enumerate(t_forg_path):
+        line = line.rstrip()
+        #sigComp2011-trainingSet-bi/016_forg/0202016_04.png
+        #sigComp2011-trainingSet-bi/016_orig/016_24.PNG
+        name = re.match(
+            r'sigComp2011-trainingSet-CN/cn_(.*?)_forg/.*', line)
+        print("index", name.group(1))
+        with open(base_dir + 'temp_lists/train_forg_' + name.group(1) + '.txt', 'a') as temp:
+            temp.write(line + '\n')
+            print(base_dir + 'temp_lists/train_forg_' + name.group(1) + '.txt')
+            print(line)
 '''
     END OF GENERATING NAME LISTS
 '''
@@ -140,11 +140,33 @@ START GENERATING NAME LISIS
     the next session of the code will iterate all lists and generating parings (positive and negative) that comes from the same name
 '''
 # auth_last = None
-with open(test_triplet_path, 'a') as test_triplet:
-    for i in range(0, len(test_forg_names)):
+# with open(test_triplet_path, 'a') as test_triplet:
+#     for i in range(0, len(test_forg_names)):
+#
+#         with open(test_forg_names[i], 'r') as forg_lists, \
+#                 open(test_auth_names[i], 'r') as auth_lists:
+#             auth_lists_itr = [line.strip() for line in auth_lists]
+#             auth_lists_itr_2 = auth_lists_itr.copy()
+#             auth_lists_itr_3 = auth_lists_itr.copy()
+#
+#             forg_lists_itr = [line.strip() for line in forg_lists]
+#
+#             # write forgery pair (1)
+#             for c1, auth_line in enumerate(auth_lists_itr):
+#                 for c2, auth_line_2 in enumerate(auth_lists_itr_2):
+#                     for c3, auth_line_3 in enumerate(auth_lists_itr_3):
+#                         test_triplet.write(auth_line + ',' + auth_line_2 + ',' + auth_line_3 + ',' + '0' + '\n')
+#
+#             for c1, auth_line in enumerate(auth_lists_itr):
+#                 for c2, auth_line_2 in enumerate(auth_lists_itr_2):
+#                     for c3, forg_line in enumerate(forg_lists_itr):
+#                         test_triplet.write(auth_line + ',' + auth_line_2 + ',' + forg_line + ',' + '1' + '\n')
 
-        with open(test_forg_names[i], 'r') as forg_lists, \
-                open(test_auth_names[i], 'r') as auth_lists:
+with open(test_triplet_path, 'a') as test_triplet:
+    for i in range(0, len(train_forg_names)):
+
+        with open(train_forg_names[i], 'r') as forg_lists, \
+                open(train_auth_names[i], 'r') as auth_lists:
             auth_lists_itr = [line.strip() for line in auth_lists]
             auth_lists_itr_2 = auth_lists_itr.copy()
             auth_lists_itr_3 = auth_lists_itr.copy()
@@ -152,12 +174,12 @@ with open(test_triplet_path, 'a') as test_triplet:
             forg_lists_itr = [line.strip() for line in forg_lists]
 
             # write forgery pair (1)
-            for c1, auth_line in enumerate(auth_lists_itr):
-                for c2, auth_line_2 in enumerate(auth_lists_itr_2):
-                    for c3, auth_line_3 in enumerate(auth_lists_itr_3):
-                        test_triplet.write(auth_line + ',' + auth_line_2 + ',' + auth_line_3 + ',' + '0' + '\n')
+            # for c1, auth_line in enumerate(auth_lists_itr):
+            #     for c2, auth_line_2 in enumerate(auth_lists_itr_2):
+            #         for c3, auth_line_3 in enumerate(auth_lists_itr_3):
+            #             test_triplet.write(auth_line + ',' + auth_line_2 + ',' + auth_line_3 + ',' + '0' + '\n')
 
             for c1, auth_line in enumerate(auth_lists_itr):
                 for c2, auth_line_2 in enumerate(auth_lists_itr_2):
                     for c3, forg_line in enumerate(forg_lists_itr):
-                        test_triplet.write(auth_line + ',' + auth_line_2 + ',' + forg_line + ',' + '1' + '\n')
+                        test_triplet.write(auth_line + ',' + auth_line_2 + ',' + forg_line + '\n')
